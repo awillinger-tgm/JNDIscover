26496cd3781ae362a01d449ee6dbf650eee15891
Charles Forcey <cforcey@historicusinc.com>
2013-03-30 00:34:05 +0100

    first passing run

---
+++ manifests/default.pp
@@ -0,0 +1,31 @@
+# update the packages on the system
+exec { "apt-get update":
+  path => "/usr/bin",
+}
+package { "apache2":
+  ensure  => present,
+  require => Exec["apt-get update"],
+}
+service { "apache2":
+  ensure  => "running",
+  require => Package["apache2"],
+}
+file { "/var/www/uptime-webpage":
+  ensure  => "link",
+  target  => "/vagrant/uptime-webpage",
+  require => Package["apache2"],
+  notify  => Service["apache2"],
+}
+# Bootstrap:
+include ldap
+# node 'server.puppetlabs.test' {
+#   class { 'ldap':
+#     server      => true,
+#     ssl         => false,
+#   }
+# }
+# ldap::define::domain {'puppetlabs.test':
+#   basedn   => 'dc=puppetlabs,dc=test',
+#   rootdn   => 'cn=admin',
+#   rootpw   => 'test',
+# }

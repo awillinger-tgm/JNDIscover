31fa9fed745c8062583140f90921360826239072
Charles Forcey <cforcey@historicusinc.com>
2013-03-30 03:21:06 +0100

    cleaning up apache test install

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,22 +2,22 @@
 exec { "apt-get update":
   path => "/usr/bin",
 }
-package { "apache2":
-  ensure  => present,
-  require => Exec["apt-get update"],
-}
-service { "apache2":
-  ensure  => "running",
-  require => Package["apache2"],
-}
-file { "/var/www/uptime-webpage":
-  ensure  => "link",
-  target  => "/vagrant/uptime-webpage",
-  require => Package["apache2"],
-  notify  => Service["apache2"],
-}
-# Bootstrap:
-# include ldap
+# package { "apache2":
+#   ensure  => present,
+#   require => Exec["apt-get update"],
+# }
+# service { "apache2":
+#   ensure  => "running",
+#   require => Package["apache2"],
+# }
+# file { "/var/www/uptime-webpage":
+#   ensure  => "link",
+#   target  => "/vagrant/uptime-webpage",
+#   require => Package["apache2"],
+#   notify  => Service["apache2"],
+# }
+
+# Bootstrap ldap
 node default {
   class { 'ldap':
     server      => true,

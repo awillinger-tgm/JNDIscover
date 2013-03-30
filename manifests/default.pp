cae6b1b8576286797999729eaa3f7add16f70a8c
Charles Forcey <cforcey@historicusinc.com>
2013-03-30 04:58:37 +0100

    cleaning out apache and canary site

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,20 +2,6 @@
 exec { "apt-get update":
   path => "/usr/bin",
 }
-# package { "apache2":
-#   ensure  => present,
-#   require => Exec["apt-get update"],
-# }
-# service { "apache2":
-#   ensure  => "running",
-#   require => Package["apache2"],
-# }
-# file { "/var/www/uptime-webpage":
-#   ensure  => "link",
-#   target  => "/vagrant/uptime-webpage",
-#   require => Package["apache2"],
-#   notify  => Service["apache2"],
-# }
 
 # Bootstrap ldap
 node default {
